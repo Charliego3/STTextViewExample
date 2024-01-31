7754e6e65ead9f20579deb140343fc17f645a781
Charlie <charlie.go.3@outlook.com>
2024-01-31 15:41:16 +0800

    Initial Commit

---
+++ STTextViewExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  STTextViewExample
+//
+//  Created by Charlie on 2024/1/31.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
